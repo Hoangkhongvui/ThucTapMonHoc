92df25d8179cd8462e1cfa2ce0156b4a6ff6de0f
Hoangkhongvui <121917433+Hoangkhongvui@users.noreply.github.com>
2025-03-20 18:05:04 +0700

    firs

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+var config = require('config');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+var controller = require(__dirname +'/app/controller');
+app.use(controller)
+var host = config.get('server.host');
+var port = config.get('server.port');
+app.listen(3000, function(){
+    console.log('Server is running on port 3000');
+})
