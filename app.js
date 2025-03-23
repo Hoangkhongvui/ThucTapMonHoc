8eecd73661080cc4a9261f3eef934d3c6c991c0a
phamminh12 <aeonepiece7a4@gmail.com>
2025-03-23 14:21:07 +0700

    Doing setup server

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const routes = require('./config/routes');
+
 var config = require('config');
 var bodyParser = require('body-parser');
-var app = express();
 
 app.use(bodyParser.json());
 
-var controller = require(__dirname +'/app/controller');
-app.use(controller)
+// var controller = require(__dirname +'/app/controllers');
+// app.use(controller)
+
+app.use(routes);
 
 var host = config.get('server.host');
 var port = config.get('server.port');
---
+++ config/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../app/controllers/productController');
+const adminController = require('../app/controllers/adminController');
+const userController = require('../app/controller/userController');
+
+
+module.exports = router;
